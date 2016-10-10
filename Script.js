58e99fdd9f1a537a84d8febae5c3780e59f2534c
Scott DeMers <scott.demers@outlook.com>
2016-10-09 19:02:45 -0600

    The dynamic nature of JavaScript and object literals

--- Script.js
+++ Script.js
@@ -1,1 +1,17 @@
-// JavaScript source code
+//strict mode shows errors and disallows deprectaed functions
+'use strict';
+
+//object literals
+var workout = {
+    title:"Strength Gym",
+    date:"10/9/2016"
+};
+
+console.log("WO Name" + workout.title);
+
+//Functions
+workout.displayDate = function () {
+    console.log("Date " + workout.date);
+}
+
+workout.displayDate();
